3996490f6422f9e0d99b53642bb566c4ec81a010
ldaijiw <lwaltmann@spartaglobal>
2020-11-17 20:06:21 +0000

    initial completion of make_booking method

--- python/booking_manager.py
+++ python/booking_manager.py
@@ -20,6 +20,88 @@ class BookingManager:
         )
         self.cursor = db_connection.cursor()
 
+    
+    # TABLES: TICKET DETAILS, FLIGHT TRIP, PASSENGERS
+    # INPUT: FLIGHT TRIP ID, LIST OF PASSENGER ID
+    def make_booking(self, flight_trip_id, passenger_id_list):
+        # CHECK HOW MANY SEATS THEY WILL NEED (BABY?)
+        passenger_discounts = []
+        passengers_need_seat = []
+        for passenger_id in passenger_id_list:
+            passenger_dob = self.cursor.execute(f'''
+            SELECT
+            DateOfBirth
+            FROM Passengers
+            WHERE Passenger_id = {passenger_id}
+            ''').fetchone()[0]
+
+            passenger_age = (date.today() - passenger_dob).days // 365
+
+            # CHECK IF THEY'RE ELIGIBLE FOR DISCOUNT
+            passenger_discounts.append(1 if passenger_age < 18 else 0)
+            # Check if they need a seat
+            passengers_need_seat.append(True if passenger_age > 1 else False)
+
+        # Calculate total seats required
+        seats_required = sum(passengers_need_seat)
+        
+        # CHECK THAT SEATS ARE AVAILABLE
+        seats_available = self.cursor.execute(f'''
+        SELECT AvailableSeats
+        FROM FlightTrip
+        WHERE FlightTrip_id = {flight_trip_id}
+        ''').fetchone()[0]
+
+        # IF SEATS ARE NOT AVAILABLE THEN RETURN: NOT AVAILABLE
+        if seats_available < seats_required:
+            return "NOT AVAILABLE"
+        
+        # OTHERWISE UPDATE NUMBER OF SEATS AVAILABLE
+        self.cursor.execute(f'''
+        UPDATE FlightTrip
+        SET AvailableSeats = {seats_available - seats_required}
+        WHERE FlightTrip_id = {flight_trip_id}
+        ''')
+        
+        # FOR EACH PASSENGER
+        # ADD COST OF TICKET TO TICKET DETAILS
+        # retrieve ticket price and ticket discount from flight trip table
+        ticket_price = self.cursor.execute(f'''
+        SELECT TicketPrice
+        FROM FlightTrip
+        WHERE FlightTrip_id = {flight_trip_id}
+        ''').fetchone()[0]
+
+        ticket_discount = self.cursor.execute(f'''
+        SELECT TicketDiscount
+        FROM FlightTrip
+        WHERE FlightTrip_id = {flight_trip_id}
+        ''').fetchone()[0]
+
+        # calculate passenger prices to pay after considering if they're eligible for discount
+        passenger_prices = [ticket_price - (ticket_price * ticket_discount * passenger_discount) for passenger_discount in passenger_discounts]
+
+        # ADD FLIGHT TRIP ID/PASSENGER ID TO TICKET DETAILS TABLE
+        ticket_id_list = []
+        for i, passenger_id in enumerate(passenger_id_list):
+            self.cursor.execute(f'''
+            INSERT INTO TicketDetails (Passenger_id, FlightTrip_id, PricePaid)
+            VALUES({passenger_id}, {flight_trip_id}, {passenger_prices[i]})
+            ''')
+
+            # retrieve ticket id and append to list
+            ticket_id = self.cursor.execute(f'''
+            SELECT Ticket_id
+            FROM TicketDetails
+            WHERE Passenger_id = {passenger_id}
+            ''').fetchone()[0]
+
+            ticket_id_list.append(ticket_id)
+
+        # RETURN: TICKET ID, COST OF TICKET (ANY DISCOUNT APPLIED), EXTRA FLIGHT DETAILS
+        return zip(ticket_id_list, passenger_prices)
+
+
     def test(self):
         self.cursor.execute('''
         CREATE TABLE [Passengers] (
@@ -45,44 +127,16 @@ class BookingManager:
 
         self.cursor.execute(insert_data_query)
         
+        insert_data_query = "INSERT INTO Passengers\n(PassportNumber, FirstName, LastName, DateOfBirth)\nVALUES('ABC123456', 'Oscar', 'Olive', '2010-01-12');"
 
+        self.cursor.execute(insert_data_query)
+        
+        insert_data_query = "INSERT INTO Passengers\n(PassportNumber, FirstName, LastName, DateOfBirth)\nVALUES('ABC123456', 'Ben', 'Button', '2019-10-13');"
 
-    
-    # TABLES: TICKET DETAILS, FLIGHT TRIP, PASSENGERS
-    def make_booking(self, flight_trip_id, passenger_id_list):
-        # INPUT: FLIGHT TRIP ID, LIST OF PASSENGER ID
-        
-        # CHECK HOW MANY SEATS THEY WILL NEED (BABY?)
-        for passenger_id in passenger_id_list:
-            passenger_dob = list(self.cursor.execute(f'''
-            SELECT
-            DateOfBirth
-            FROM Passengers
-            WHERE Passenger_id = {passenger_id}
-            ''').fetchall()[0])[0]
+        self.cursor.execute(insert_data_query)
 
-            passenger_age = date.today() - passenger_dob
-        
-        print(passenger_dob)
-        print(type(passenger_dob))
-        print(passenger_age)
-        
-
-            
-        # CHECK THAT SEATS ARE AVAILABLE
-        # IF SEATS ARE NOT AVAILABLE THEN RETURN: NOT AVAILABLE
-        # OTHERWISE UPDATE NUMBER OF SEATS AVAILABLE
-
-        # CHECK IF THEY'RE ELIGIBLE FOR DISCOUNT
-        
-        # FOR EACH PASSENGER
-        # ADD FLIGHT TRIP ID/PASSENGER ID TO TICKET DETAILS TABLE
-        # ADD COST OF TICKET TO TICKET DETAILS
-        
-        # RETURN: TICKET ID, COST OF TICKET (ANY DISCOUNT APPLIED), EXTRA FLIGHT DETAILS
-        pass
 
 if __name__ == "__main__":
     new_bm = BookingManager()
     new_bm.test()
-    new_bm.make_booking(1, [1])
+    new_bm.make_booking(1, [1, 2, 3])
