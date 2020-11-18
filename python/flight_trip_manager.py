de90b9483654e46baf2713218fa37a54b5f1f21b
samturton2 <STurton@spartaglobal.com>
2020-11-18 10:38:31 +0000

    change_aircraft function completed

--- python/flight_trip_manager.py
+++ python/flight_trip_manager.py
@@ -25,9 +25,9 @@ class FlightTripManager:
         INSERT INTO FlightTrip (Aircraft_id, DepartureTime, ArrivalTime, AvailableSeats, DepartureAirport, ArrivalAirport, TicketPrice, TicketDiscount)
         VALUES ( -1, {DepartureTime.strftime('%Y-%m-%d %H:%M:%S')}, {ArrivalTime.strftime('%Y-%m-%d %H:%M:%S')}, -1, 'LHR', '{ArrivalAirport}', {TicketPrice}, {TicketDiscount});
         """)
-        # self.db_connection.commit()
-        ## UNFINISHED
-        FlightTrip_id = list(self.cursor.execute(f"SELECT FlightTrip_id FROM FlightTrip WHERE {???} ").fetchone())[0]
+        self.db_connection.commit()
+        # Attempt to collect the last inserted flight trip identity
+        FlightTrip_id = list(self.cursor.execute(f"SELECT FlightTrip_id FROM FlightTrip WHERE @@IDENTITY;").fetchone())[0]
         # RETURN: FLIGHT TRIP ID
         return FlightTrip_id
 
@@ -52,7 +52,7 @@ class FlightTripManager:
                             WHERE Aircraft.Aircraft_id = {Aircraft_id};
                             """).fetchone())[0]
             self.cursor.execute(f"UPDATE FlightTrip SET AvailableSeats = {max_capacity} WHERE Aircraft_id = {Aircraft_id};")
-            # self.db_connection.commit()
+            self.db_connection.commit()
             # RETURN: AIRCRAFT ID
             return int(Aircraft_id)
         else:
@@ -61,17 +61,21 @@ class FlightTripManager:
     # TABLES: FLIGHT TRIP, AIRCRAFT
     def change_aircraft(self, FlightTrip_id):
         # INPUT: FLIGHT TRIP ID
-        # INPUT: FLIGHT TRIP ID
-        trip_id = self.cursor.execute(f"SELECT FlightTrip_id FROM FlightTrip WHERE FlightTrip_id = {FlightTrip_id} ;")
+        trip_id = self.cursor.execute(f"SELECT FlightTrip_id FROM FlightTrip WHERE FlightTrip_id = {FlightTrip_id};")
         # CHECK THAT IT's A VALID FLIGHT TRIP ID
         if len(list(trip_id)) == 1:
-        try:
-            # COLLECT CURRENT AIRPLANE ID IF ITS BEEN ASSIGNED AN AIRPLANE
-            Aircraft_id = list(self.cursor.execute("SELECT Aircraft_id FROM FlightTrip_id WHERE AircraftStatus_id = 0;").fetchone())[0]
-        Except
-        # CALL ASSIGN_AIRCRAFT() AGAIN
-        newAircraft_id = self.assign_aircraft(FlightTrip_id)
-        # NEW PLANE ID = assign_aircraft()
+            try:
+                # COLLECT CURRENT AIRPLANE ID IF ITS BEEN ASSIGNED AN AIRCRAFT
+                Aircraft_id = list(self.cursor.execute(f"SELECT Aircraft_id FROM FlightTrip_id WHERE FlightTrip_id = {FlightTrip_id};").fetchone())[0]
+                # CALL ASSIGN_AIRCRAFT() AGAIN
+                newAircraft_id = self.assign_aircraft(FlightTrip_id)
 
-        # RETURN: NEW AIRCRAFT ID
-        return newAircraft_id
+            except:
+                return "This airplane hasn't been assigned an aircraft, we will do that now."
+            else:
+                self.cursor.execute(f"UPDATE Aircraft SET AircraftStatus_id = 0 WHERE Aircraft_id = {Aircraft_id};")
+            finally:
+                # RETURN: NEW AIRCRAFT ID
+                return newAircraft_id
+        else:
+            return "something went wrong, perhaps incorrect FlightTrip id"
