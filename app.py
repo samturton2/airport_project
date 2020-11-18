453e97ae2de0c50772b9e5d69bf6e145d8ada936
ldaijiw <lwaltmann@spartaglobal>
2020-11-18 22:39:26 +0000

    started work on flask, initial login to staff page working

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify, redirect, url_for, render_template, request, session
+from classes.Log_in_display import LogIn
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/staffhome/")
+def staffhome():
+    return render_template('staff_home.html')
+
+
+@app.route("/aboutus/")
+def about_us():
+    return render_template('aboutus.html')
+
+# if any error occurs then redirects to error message page
+@app.errorhandler(Exception)
+def error_occured(error):
+    return render_template('error.html')
+
+
+
+
+@app.route("/loginpage/", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print("POST")
+        #session.pop('user_id')
+        r_username = request.form['username']
+        r_password = request.form['password']
+
+        
+        test_login = LogIn()
+
+        try:
+            test_username, test_password = test_login.log_in(r_username, r_password)
+        except:
+            return redirect(url_for('login'))
+        
+        else:
+            if r_username == test_username and r_password == test_password:
+                return redirect('/staffhome/')
+            return redirect(url_for('login'))
+
+        return redirect(url_for('login'))
+
+    return render_template("login.html")
+
+
+'''
+FLIGHT TRIP MANAGER
+'''
+
+@app.route("/ftm/")
+def flighttripmanager():
+    return render_template('ftm_home.html')
+
+
+'''
+BOOKING MANAGER
+'''
+
+app.run(debug=True)
